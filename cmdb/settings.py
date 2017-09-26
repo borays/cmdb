a24fdb4f4659d7fe953902efd0a6964e15865c01
ulric <borays@qq.com>
2017-09-26 16:51:21 +0800

    0.1

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'cmdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
-#}
+# }
 
 DATABASES = {
     'default': {
